99dd73b95b8363eaa5c00777d804138c3edcbcd1
Saiful-hoque <haquesaiful705@gmail.com>
2026-09-02 22:26:58 +0600

    Fix media static files for production

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', include('portfolio.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# DEBUG True বা False যাই হোক না কেন, মিডিয়া ফাইল শো করবে
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
